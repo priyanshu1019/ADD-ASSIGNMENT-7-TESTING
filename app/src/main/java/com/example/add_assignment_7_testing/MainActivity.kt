8d0aa4326cda70a43a9a68afd877f2cbdd5d8ea8
priyanshu1019 <goutampriyanshu27@gmail.com>
2024-06-20 10:21:47 +0530

    created the validate function in LoginUtils file

---
+++ app/src/main/java/com/example/add_assignment_7_testing/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gaur.testing
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.add_assignment_7_testing.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/com/example/add_assignment_7_testing/MainActivityTest.kt
@@ -0,0 +1,14 @@
+package com.example.add_assignment_7_testing
+
+import org.junit.jupiter.api.Assertions.*
+
+class MainActivityTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    fun setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    fun tearDown() {
+    }
+}
