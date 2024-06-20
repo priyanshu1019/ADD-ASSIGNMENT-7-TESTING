494aa43d224c7dea016715301f3dd895fa6e480b
priyanshu1019 <goutampriyanshu27@gmail.com>
2024-06-20 10:24:01 +0530

    Completed testing functionality in our app , with all validations

--- app/src/test/java/com/example/add_assignment_7_testing/MainActivityTest.kt
+++ app/src/test/java/com/example/add_assignment_7_testing/MainActivityTest.kt
@@ -1,14 +1,44 @@
-package com.example.add_assignment_7_testing
+package com.gaur.testing
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
 
 class MainActivityTest {
 
-    @org.junit.jupiter.api.BeforeEach
+    @Before
     fun setUp() {
     }
 
-    @org.junit.jupiter.api.AfterEach
+    @After
     fun tearDown() {
     }
+
+    @Test
+    fun username_is_empty() {
+        val username = ""
+        val assert = LoginUtils.validate(username, "dflja@3", "dkjafjla", 20)
+        assert(assert == "please enter username")
+    }
+
+
+    @Test
+    fun username_length_less_than_6() {
+        val username = "djlfa"
+        assert(
+            LoginUtils.validate(username, "dflja@3", "dkjafjla", 20)
+                    == "very short username"
+        )
+    }
+
+    @Test
+    fun username_length_very_long(){
+        val username="fdlksakjfklafjkjfklkaljfklajfkajfkjasldkjfajlksfjlakjfakl"
+        assert(
+            LoginUtils.validate(username, "dflja@3", "dkjafjla", 20)
+                    == "long username"
+        )
+    }
+
+
 }
